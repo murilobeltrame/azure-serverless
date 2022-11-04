c7f89c07610ee8368e67eddf5a8eb1f937f51aca
Murilo Beltrame <murilobeltrame@gmail.com>
2022-11-04 00:51:53 -0300

    ....

--- src/backend/RecorderFunctions/Functions/DisableInactiveRecordsFunction.cs
+++ src/backend/RecorderFunctions/Functions/DisableInactiveRecordsFunction.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace RecorderFunctions.Functions
@@ -8,9 +11,29 @@ namespace RecorderFunctions.Functions
     public class DisableInactiveRecordsFunction
     {
         [FunctionName("DisableInactiveRecordsFunction")]
-        public void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public async Task Run(
+            [TimerTrigger("0 */5 * * * *")] TimerInfo myTimer,
+            [CosmosDB(
+                databaseName: "Records",
+                collectionName: "People",
+                ConnectionStringSetting = "CosmosDbConnection"
+            )] DocumentClient client,
+            ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var collectionUri = UriFactory.CreateDocumentCollectionUri("Records", "People");
+            var query = client.CreateDocumentQuery<Person>(collectionUri)
+                .Where(w => w.CreatedAt < DateTime.Now.Subtract(TimeSpan.FromMinutes(5)))
+                .AsDocumentQuery();
+
+            while (query.HasMoreResults)
+            {
+                foreach (var person in await query.ExecuteNextAsync())
+                {
+                    person.Active = false;
+                }
+            }
         }
     }
 }
--- src/backend/RecorderFunctions/Models/Person.cs
+++ src/backend/RecorderFunctions/Models/Person.cs
@@ -1,8 +1,12 @@
-﻿namespace RecorderFunctions
+﻿using System;
+
+namespace RecorderFunctions
 {
     public class Person
     {
         public string Name { get; set; }
         public string Phone { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool Active { get; set; } = true;
     }
 }
